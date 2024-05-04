77e2d98272818cc859593b555d6df90f07a46418
mahmoudhafez103 <hafz7062@gmail.com>
2024-05-04 21:22:51 +0300

    vid 42 translation

--- resources/views/components/post-idea.blade.php
+++ resources/views/components/post-idea.blade.php
@@ -1,21 +1,22 @@
 @auth()
-
-<h4> Share yours ideas </h4>
-<div class="row">
-    <form action="/ideas" method="POST" enctype="multipart/form-data">
-        @csrf
-        <div class="mb-3">
-            <textarea class="form-control" id="content" name="content" rows="3"></textarea>
-            @error('content')
-                <span class="fs-6 text-danger">{{ $message }}</span>
-            @enderror
-        </div>
-        <div class="">
-            <button type="submit" class="btn btn-dark"> Share </button>
-        </div>
-    </form>
-</div>
+    <h4> Share yours ideas </h4>
+    <div class="row">
+        <form action="/ideas" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="mb-3">
+                <textarea class="form-control" id="content" name="content" rows="3"></textarea>
+                @error('content')
+                    <span class="fs-6 text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="">
+                <button type="submit" class="btn btn-dark"> Share </button>
+            </div>
+        </form>
+    </div>
 @endauth
 @guest
-    <h4>Login To Share yours ideas </h4>
+    <h4> {{ __('ideas.login_to_share') }} </h4>
+    {{-- <h4> {{ trans('ideas.login_to_share') }} </h4>
+    <h4> @lang('ideas.login_to_share') </h4> --}}
 @endguest
